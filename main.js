cdaf20716b017422627ad4cc974414af7077bcdb
aninha0908 <matta.ana@escola.pr.gov.br>
2026-05-13 15:34:38 -0300

    2026

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes=document.querySelectorAll(".botao");
+console.log(botoes);
+
+for (let i=0; i<= botoes.length; i++){
+    botoes[i].onclick=function{
+
+        for (let i=0;i;j<botoes.length;j++){
+          botoes[j].classList.remove("ativo");  
+        }
+        botoes[i].classList.add("ativo");
+    }
+console.log(botoes[i]);
+}
